e8facf88d74b83a901399bb4ce7479d19761ac8b
kkya <kkya2014@163.com>
2015-06-11 11:15:02 +0800

    no message

---
+++ co-modules/data-index.js
@@ -0,0 +1,96 @@
+var data = [{
+                                                    cont : "按钮",
+                                                    name : "control",
+                                                    tar : "examples/button.html"
+                                            },{
+                                                    cont : '多选框',
+                                                    name : "listview",
+                                                    tar : "examples/checkbox.html"
+                                            },{
+                                                    cont : '弹出框',
+                                                    name : "listview",
+                                                    tar : "examples/dialog.html"
+                                                },{
+                                                    cont : '列表',
+                                                    name : "listview",
+                                                    tar : "examples/gridview.html"
+                                                },{
+                                                    cont : '导航',
+                                                    name : "listview",
+                                                    tar : "examples/header.html"
+                                                },{
+                                                    cont : '文本框',
+                                                    name : "listview",
+                                                    tar : "examples/input.html"
+                                                },{
+                                                    cont : '单选框',
+                                                    tar : "examples/radio.html"
+                                                },{
+                                                    cont : '下拉列表',
+                                                    name : "listview",
+                                                    tar : "examples/select.html"
+                                                },{
+                                                    cont : '轮播图',
+                                                    name : "listview",
+                                                    tar : "examples/slider.html"
+                                                },{
+                                                    cont : '开关',
+                                                    name : "listview",
+                                                    tar : "examples/switche.html"
+                                                },{
+                                                    cont : "Native",
+                                                    contents : [{
+                                                        cont : 'accelerometer',
+                                                        tar : "accelerometer.html"
+                                                    },{
+                                                        cont : 'alert',
+                                                        tar : "alert.html"
+                                                    },{
+                                                        cont : 'audio',
+                                                        tar : "audio.html"
+                                                    },{
+                                                        cont : 'barcode',
+                                                        tar : "barcode.html"
+                                                    },{
+                                                        cont : 'camera',
+                                                        tar : "camera.html"
+                                                    },{
+                                                        cont : 'contacts',
+                                                        tar : "contacts.html"
+                                                    },{
+                                                        cont : 'databace',
+                                                        tar : "databace.html"
+                                                    },{
+                                                        cont : 'device',
+                                                        tar : "device.html"
+                                                    },{
+                                                        cont : 'download',
+                                                        tar : "download.html"
+                                                    },{
+                                                        cont : 'gallery',
+                                                        tar : "gallery.html"
+                                                    },{
+                                                        cont : 'geolocation',
+                                                        tar : "geolocation.html"
+                                                    },{
+                                                        cont : 'storage',
+                                                        tar : "storage.html"
+                                                    },{
+                                                        cont : 'testLog',
+                                                        tar : "testLog.html"
+                                                    },{
+                                                        cont : 'windowTest',
+                                                        tar : "windowTest.html"
+                                                    },{
+                                                        cont : 'zip',
+                                                        tar : "zip.html"
+                                                    }]
+                                                },{
+                                                    cont : 'Tab bar',
+                                                    name : "listview",
+                                                    tar : "examples/tabs.html"
+                                                },{
+                                                    cont : '树形列表',
+                                                    name : "listview",
+                                                    tar : "examples/treeview.html"
+                                                }];			
